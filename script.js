aceeb54b8cadc5715ea27cf5da3d8032dd294444
shalomjaison <sjacobjaison@gmail.com>
2025-07-27 23:45:58 -0400

    Added Page 2 + started page 3

---
+++ script.js
@@ -0,0 +1,58 @@
+let scrollY = 0;
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('#main'),
+  smooth: true
+});
+
+scroll.on("scroll", (args) => {
+  scrollY = args.scroll.y;
+});
+
+const listItems = document.querySelectorAll('.list-item');
+const hoverImg = document.querySelector('.hover-img');
+
+let mouseX = 0;
+let mouseY = 0;
+let currentX = 0;
+let currentY = 0;
+
+document.addEventListener('mousemove', (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY + scrollY;
+});
+
+listItems.forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        const img = item.getAttribute('data-img');
+        hoverImg.style.backgroundImage = `url(${img})`
+        hoverImg.style.opacity = 1;
+
+    });
+    item.addEventListener('mouseleave', () => {
+        hoverImg.style.opacity = 0
+    })
+})
+
+function animate() {
+    currentX += (mouseX - currentX) * 0.3;
+    currentY += (mouseY - currentY) * 0.3;
+
+    const deltaX = mouseX - currentX;
+    const deltaY = mouseY - currentY;
+
+    const skewX = deltaX * 0.2;
+    const skewY = deltaY * 0.2;
+
+    hoverImg.style.left = `${currentX}px`;
+    hoverImg.style.top = `${currentY}px`;
+    hoverImg.style.transform = `translate(5%, 5%) skew(${skewX}deg, ${skewY}deg)`;
+
+    console.log(`mouseY: ${mouseY}, scrollY: ${scrollY}, currentY: ${currentY}`);
+    requestAnimationFrame(animate);
+
+
+}
+
+
+animate()
